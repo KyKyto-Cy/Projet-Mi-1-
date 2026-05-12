4edda049b236fd1e4d2bc19466b97419af717220
Kamil Benslimane <b.kamil2707g@gmail.com>
2026-05-12 22:38:19 +0200

    couleur

--- src/display.h
+++ src/display.h
@@ -6,9 +6,15 @@
 #include "board.h"
 #include "player.h"
 
-
-
-
+#define RESET       "\033[0m"
+#define ROUGE       "\033[31m"
+#define VERT        "\033[32m"
+#define JAUNE       "\033[33m"
+#define BLEU        "\033[34m"
+#define MAGENTA     "\033[35m"
+#define CYAN        "\033[36m"
+#define BLANC       "\033[37m"
+#define GRAS        "\033[1m"
 
 
 
