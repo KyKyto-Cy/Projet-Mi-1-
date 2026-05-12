6ab487884324713a0dfc964f6acec10971b8000e
Kamil Benslimane <b.kamil2707g@gmail.com>
2026-05-12 23:34:21 +0200

    affichage case

--- src/display.c
+++ src/display.c
@@ -3,13 +3,57 @@
 
 void afficher_case(Case c) {
     if (c.revelee == 0){
-        printf(BLEU "[ ? ]" RESET);
+        printf(BLEU "[ ? ]" RESET); // affiche la case en bleu si la case n'est pas révélée
         return;
     }
-}
+
 
 switch (c.type){
     case VIDE:
-    printf(BLANC "[   ]" RESET);
+    printf(BLANC "[   ]" RESET); //affiche la case en blanc si le type de la case est VIDE
     break;
+
+    case MONSTRE:
+    switch (c.monstre) {
+        case BASILIC :
+        printf(ROUGE "[ B ]" RESET); //affiche la lettre B sur la case en rouge si le monstre est un basilic
+        break;
+
+        case ZOMBIE:
+        printf(ROUGE "[ Z ]" RESET);// affiche la lettre Z sur la case en rouge si le monstre est un zombie
+        break;
+
+        case TROLL:
+        printf(ROUGE "[ T ]" RESET);// affiche la lettre T sur la case en rouge si le monstre est un troll
+        break;
+
+        case HARPIE:
+        printf(ROUGE "[ H ]" RESET);// affiche la lettre H sur la case en rouge si le monstre est un harpie
+        break;
+    }
+    break;
+
+    case COFFRE:
+    printf(JAUNE "[ C ]" RESET);
+    break;
+
+    case ARME_ANTIQUE:
+    printf(MAGENTA "[ A ]" RESET);
+    break;
+
+    case PORTAIL:
+    printf(CYAN "[ P ]"RESET);
+    break;
+
+    case TOTEM :
+    printf(VERT "[ T ]" RESET);
+    break;
+
+
+
+
 }
+
+}
+
+
