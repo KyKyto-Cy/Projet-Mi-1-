a462e38a5b207721919394786e148f1bd082ddf3
Kamil Benslimane <b.kamil2707g@gmail.com>
2026-05-13 12:58:41 +0200

    menu.h

--- src/menu.h
+++ src/menu.h
@@ -1,6 +1,14 @@
 #ifndef MENU_H
 #define MENU_H
 
+
+#include <board.h>
+#include <player.h>
+
+void menu_principal();
+void choisir_joueur();
+void menu_final();
+
 // TODO: prototypes des fonctions du module menu
 
 #endif
