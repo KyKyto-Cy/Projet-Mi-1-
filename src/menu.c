4a89d599bc0e2acf3339e5f6adca4813a5e9a07b
Kamil Benslimane <b.kamil2707g@gmail.com>
2026-05-13 13:17:29 +0200

    affichage menu

---
+++ src/menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+void menu_principal(){
+    printf("==================================================== \n");
+    printf("===                                              === \n");
+    printf("===                    MEMO RPG                  === \n");
+    printf("===                                              === \n");
+    printf("===                                              === \n");
+    printf("===                                              === \n");
+    printf("===                                              === \n");
+    printf("===                NOUVELLE PARTIE               === \n");
+    printf("===                                              === \n");
+    printf("===                                              === \n");
+    printf("===                                              === \n");
+    printf("===                                              === \n");
+    printf("===  QUITTER                                     === \n");
+    printf("==================================================== \n");
+
+
+
+}
