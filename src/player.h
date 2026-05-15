61383bcd3e62ac1bc60ae11de437f88e16046de0
Kamil Benslimane <b.kamil2707g@gmail.com>
2026-05-15 15:45:24 +0200

    strutures enum type aventurier, structure Joueur

--- src/player.h
+++ src/player.h
@@ -3,6 +3,22 @@
 
 // TODO: prototypes des fonctions du module player
 
+typedef enum{
+    GUERRIER,
+    RANGER,
+    MAGICIEN, 
+    VOLEUR,
+} TypeAventurier;
+
+
+typedef struct {
+    char nom[50];
+    TypeAventurier type;
+    int x, y; //position sur le tableau
+    int a_coffre; //0= non , 1= oui
+    int a_arme;//0 = non , 1 = oui
+
+} Joueur;
 
 
 #endif
